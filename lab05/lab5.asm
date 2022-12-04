8f47a23b52848c1301eba33348a837b97a71b47f
sadjyakonova <114990796+sadjyakonova@users.noreply.github.com>
2022-12-04 18:48:07 +0300

    Add files via upload

---
+++ lab05/lab5.asm
@@ -0,0 +1,15 @@
+; lab5.asm
+SECTION .data ; Начало секции данных
+	lab5: DB 'Dyakonova Sophya',10
+	lab5Len: EQU $-lab5 ; Длина строки lab5
+SECTION .text ; Начало секции кода
+	GLOBAL _start
+_start: ; Точка входа в программу
+	mov eax,4 ; Системный вызов для записи (sys_write)
+	mov ebx,1 ; Описатель файла '1' - стандартный вывод
+	mov ecx,lab5 ; Адрес строки lab5 в ecx
+	mov edx,lab5Len ; Размер строки lab5
+	int 80h ; Вызов ядра
+	mov eax,1 ; Системный вызов для выхода (sys_exit)
+	mov ebx,0 ; Выход с кодом возврата '0' (без ошибок)
+	int 80h ; Вызов ядра
